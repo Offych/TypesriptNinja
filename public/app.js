5d643996e7c7b69691ecc46d755e418ea6856ddf
Alexander Smotrikov <asmotrikov@fbsdata.com>
2021-03-01 16:10:03 +0300

    Classes creation, instances

--- public/app.js
+++ public/app.js
@@ -5,6 +5,28 @@
 }
  */
 //console.log(anchor.href);
+//Classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invoiceOne = new Invoice('Sashiko', 'some work', 250);
+var invoiceTwo = new Invoice('Dimiko', 'some additional work', 300);
+var invoices = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+invoiceOne.client = 'Leshka';
+invoiceTwo.amount = 500;
+console.log(invoiceOne, invoiceTwo);
+console.log(invoices);
+//console.log(invoiceOne, invoiceTwo);
 //const form = document.querySelector('form')!;
 var form = document.querySelector('.new-item-form');
 //console.log(form.children);
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,40 @@
  */
 //console.log(anchor.href);
 
+//Classes
+
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+    
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+
+const invoiceOne = new Invoice('Sashiko', 'some work', 250);
+const invoiceTwo = new Invoice('Dimiko', 'some additional work', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+
+invoiceOne.client = 'Leshka';
+invoiceTwo.amount = 500;
+console.log(invoiceOne, invoiceTwo);
+
+console.log(invoices);
+
+//console.log(invoiceOne, invoiceTwo);
+
+
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -23,4 +57,7 @@ form.addEventListener('submit', (e: Event) => {
         details.value,
         amount.valueAsNumber
     );
-})
+});
+
+
+
